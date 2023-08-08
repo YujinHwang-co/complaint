c9090d275aceadc83e608b26e5e97cfcf7b63da1
YujinHwang-co <bineex2@naver.com>
2023-08-08 15:30:15 +0900

    chore: spring security 로그인 페이지 비활성화

--- src/main/java/com/personal/complaint/ComplaintApplication.java
+++ src/main/java/com/personal/complaint/ComplaintApplication.java
@@ -2,8 +2,9 @@ package com.personal.complaint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class ComplaintApplication {
 
 	public static void main(String[] args) {
