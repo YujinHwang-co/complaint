a650fd341a5cc33fd6c41b06ea06eea912cfd5b7
YujinHwang-co <bineex2@naver.com>
2023-08-16 18:42:47 +0900

    fix: 회원가입 컬럼 추가, insert 쿼리 수정

--- src/main/java/com/personal/complaint/server/model/MbrInfoVo.java
+++ src/main/java/com/personal/complaint/server/model/MbrInfoVo.java
@@ -14,9 +14,21 @@ public class MbrInfoVo {
     @Schema(description = "회원 ID")
     @NotBlank
     String mbrId;
+    @Schema(description = "회원 이름")
+    String mbrNm;
+    @Schema(description = "휴대폰번호")
+    String mbtlnum;
+    @Schema(description = "우편번호")
+    String zip;
+    @Schema(description = "주소")
+    String addr;
+    @Schema(description = "상세주소")
+    String addrDetail;
     @Schema(description = "비밀번호")
     @NotBlank
     String password;
     @Schema(description = "가입 일시")
     Date joinDt;
+    @Schema(description = "최종 수정 일시")
+    Date lastMdfDt;
 }
