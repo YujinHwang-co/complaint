7c691cc5ec373e4a17f6d229ddc23d0d78042504
YujinHwang-co <bineex2@naver.com>
2023-08-20 16:01:00 +0900

    chore: gitignore 설정, DB 변경, log 설정, SecurityConfig 변경
    1. gitignore에 log파일 모두 무시
    2. DB h2에서 mariaDB로 변경
    3. log4j2 local console에 sql문 확인 가능하도록 설정
    4. SecurityFilterChain 빈 등록

--- src/main/java/com/personal/complaint/server/core/config/SecurityConfig.java
+++ src/main/java/com/personal/complaint/server/core/config/SecurityConfig.java
@@ -3,10 +3,14 @@ package com.personal.complaint.server.core.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -20,11 +24,12 @@ public class SecurityConfig {
 
     /**
      * 아래의 requestMatchers 요청은 security 무시
+     *
      * @return
      */
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        String[] pathsStr ={
+        String[] pathsStr = {
                 "/v3/api-docs",
                 "/configuration/ui",
                 "/swagger-resources",
@@ -33,9 +38,25 @@ public class SecurityConfig {
                 "/swagger-ui.html",
                 "/configuration/security",
                 "/webjars/**",
-                "/swagger/**",
-                "/h2-console/**"
+                "/swagger/**"
         };
         return (web) -> web.ignoring().requestMatchers(pathsStr);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                // 시큐리티는 기본적으로 세션을 사용
+                .sessionManagement((sessionManagement) ->
+                        // 세션 설정을 Stateless 로 설정
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("/api/admin/auth/**").permitAll()
+                                .anyRequest().permitAll()
+                );
+
+        return http.build();
     }
 }
