e105d0212ebf2f9bc73572aac9169ef8448b8a00
YujinHwang-co <bineex2@naver.com>
2023-10-27 14:36:45 +0900

    chore: SecurityConfig 수정
    - hasRole 로직 확인 필요

--- src/main/java/com/personal/complaint/server/core/config/SecurityConfig.java
+++ src/main/java/com/personal/complaint/server/core/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.personal.complaint.server.core.config;
 
+import com.personal.complaint.server.core.jwt.JwtAccessDeniedHandler;
+import com.personal.complaint.server.core.jwt.JwtAuthenticationEntryPoint;
+import com.personal.complaint.server.core.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +20,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final TokenProvider tokenProvider;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -24,12 +31,10 @@ public class SecurityConfig {
 
     /**
      * 아래의 requestMatchers 요청은 security 무시
-     *
-     * @return
      */
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        String[] pathsStr = {
+        String[] pathStr = {
                 "/v3/api-docs",
                 "/configuration/ui",
                 "/swagger-resources",
@@ -40,23 +45,35 @@ public class SecurityConfig {
                 "/webjars/**",
                 "/swagger/**"
         };
-        return (web) -> web.ignoring().requestMatchers(pathsStr);
+        return (web) -> web.ignoring().requestMatchers(pathStr);
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                // 시큐리티는 기본적으로 세션을 사용
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token 사용하는 방식이기 때문에 csrf disable
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling((exceptionHandling) ->
+                        exceptionHandling
+                                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
                 .sessionManagement((sessionManagement) ->
-                        // 세션 설정을 Stateless 로 설정
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests
-                                .requestMatchers("/api/admin/auth/**").permitAll()
-                                .anyRequest().permitAll()
-                );
+                // HttpServletRequest를 사용하는 요청들에 대한 접근제한 설정
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                                .requestMatchers("/api/auth/login").permitAll() // 로그인 api
+                                .requestMatchers("/api/mbr/insertMbrInfoVo").permitAll() // 회원가입 api
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/mbr/listMbrInfo").hasRole("ADMIN") // TEST
+                                .anyRequest().permitAll() // 그 외 인증 없이 접근X **permitAll -> authenticated**
+                )
+                // JwtFilter를 addFilterBefore로 등록했던 JwtSecurityConfig class 적용
+                .apply(new JwtSecurityConfig(tokenProvider));
 
-        return http.build();
+        return httpSecurity.build();
     }
 }
